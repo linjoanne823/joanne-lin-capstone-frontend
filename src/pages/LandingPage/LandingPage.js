6d5713b158ca73fc9d600f176217749a13ec5b7e
Joanne <linjoanne823@gmail.com>
2022-08-25 09:38:38 -0700

    Added responsivenss for landing page and home page

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -2,7 +2,7 @@ import React from "react";
 import DropdownMenu from "../../components/DropdownMenu/DropdownMenu";
 import Header from "../../components/Header/Header";
 import CssBaseline from "@mui/material/CssBaseline";
-import { Typography, Box, Container } from "@mui/material";
+import { Typography, Box, Container, Grid } from "@mui/material";
 
 const LandingPage = (props) => {
   return (
@@ -15,12 +15,13 @@ const LandingPage = (props) => {
               sx={{
                 backgroundImage: `url('https://cdn.pixabay.com/photo/2017/11/08/22/18/spaghetti-2931846_1280.jpg')`,
                 height: "100vh",
-                width:"100vw",
+                width:"75vw",
                 margin:0,
-                backgroundRepeat:"no-repeat"
+                backgroundRepeat:"no-repeat",
+                objectFit:"cover"
               }}
             >
-              <Typography style={{ color: "white", display:'flex', justifyContent:"flex-start", fontSize:"4rem", paddingLeft:"1rem"}}>FoodieGram</Typography>
+              <Typography style={{ color: "white", display:'flex', justifyContent:"flex-start", fontSize:"4rem", paddingLeft:"1rem", width:"5rem", wordWrap:"normal"}}>Foodie Gram</Typography>
               <Typography style={{ color: "white", display:'flex', justifyContent:"flex-start", fontSize:"2rem", paddingLeft:"1rem", width:"10rem"}}>The only website you need to satisfy your cravings</Typography>
 
             </Box>
